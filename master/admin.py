7275dfad2fa03d66334f2146123217e31aa09c94
kashyap5113 <parmarkashyap104@gmail.com>
2025-12-02 17:24:27 +0530

    devbhhomi1

--- master/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from . import models
-
-# Automatically register all models in master module
-for model in models.__all__:
-    admin.site.register(getattr(models, model))
